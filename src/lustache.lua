1e2c59c896e5eb8d36c36c3677a82bab5c1f5105
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2023-01-25 04:17:24 +0100

    adapt import

--- src/lustache.lua
+++ src/lustache.lua
@@ -13,7 +13,7 @@ end
 local lustache = {
   name     = "lustache",
   version  = "1.3.1-0",
-  renderer = require("lustache.renderer"):new(),
+  renderer = require("Module:Lustache/Renderer"):new(),
 }
 
 return setmetatable(lustache, {
